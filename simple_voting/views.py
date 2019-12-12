baf1cb74983d959a634cffec14df4c550d8f280c
Andrew Tkachev <sim200347@gmail.com>
2019-12-12 22:26:39 +0300

    Add base pages and login page for future development

--- simple_voting/views.py
+++ simple_voting/views.py
@@ -1,3 +1,36 @@
+import datetime
+
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from simple_voting.forms import Complain
+
+
+def index(request):
+    context = {}
+    context['data'] = datetime.datetime.now()
+
+    return render(request, 'index.html', context)
+
+
+def available_voting(request):
+    context = {}
+    context['data'] = datetime.datetime.now()
+
+    return render(request, 'available_voting.html', context)
+
+
+def complain(request):
+    context = {}
+    if request.method == 'POST':
+        cmpl = Complain(request.POST)
+        if cmpl.is_valid():
+            context['username'] = cmpl.username
+            context['complain_type'] = cmpl.complain_type
+            context['description'] = cmpl.description
+            context['date'] = cmpl.date
+            context['form'] = cmpl
+        else:
+            context['form'] = cmpl
+
+    return render(request, 'complain.html', context)
