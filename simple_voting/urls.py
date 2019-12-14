42041d932a5bd41f4b625875fe63d10c74ee883d
Савинкин Юрий <savinkin@informatics.ru>
2019-12-14 20:28:01 +0300

    fix urls

--- simple_voting/urls.py
+++ simple_voting/urls.py
@@ -13,14 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include,path
-
-from simple_voting import views
-from django.contrib.auth import views as auth_views
+from django.urls import path
+from .views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('simple_voting.urls')),
-    path('login/', auth_views.LoginView.as_view()),
+    path('', index),
+    path('available_voting/', available_voting),
 ]
