544cc7a72f2da1e64908d29ebf4c3703aa9c28dd
Sergey Gordeev <duamaster1504@yandex.ru>
2019-12-20 23:14:33 +0300

    Added the server part of the registration mechanism

--- simple_voting/forms.py
+++ simple_voting/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class Complain(forms.Form):
@@ -6,3 +8,11 @@ class Complain(forms.Form):
     complain_type = forms.IntegerField(label='Your complain type: ', required=True)
     description = forms.IntegerField(label='Describe your problem: ', required=False)
     date = forms.DateField(label='Date: ')
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=250, help_text='Обязательное поле.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
