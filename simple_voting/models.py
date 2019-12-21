0f49079f85de8ae2fd9ac0baafcb6aed61127bae
Nikita Gudkov <nikitaster2001@gmail.com>
2019-12-21 18:01:41 +0300

    add vote model

--- simple_voting/models.py
+++ simple_voting/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Voting(models.Model):
     question = models.CharField(max_length=255)
-    author = models.IntegerField(null=False, default=0)
-    description = models.CharField(max_length=255)
+    author = models.IntegerField(null=False)
+    description = models.CharField(max_length=255, null=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
@@ -16,5 +17,10 @@ class Option(models.Model):
     text = models.CharField(max_length=50)
     voting = models.ForeignKey(to=Voting, on_delete=models.CASCADE)
 
+
+class Vote(models.Model):
+    option_id = models.ForeignKey(to=Option, on_delete=models.CASCADE)
+    author_id = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
 
 
